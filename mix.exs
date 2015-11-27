8206ae189ff4e67a6e3755570914e9db09ab5249
Josh Crews <josh@joshcrews.com>
2015-11-27 11:11:51 -0600

    trying to get on edge of phoenix

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule ReactWebpack.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 1.0.2"},
+    [{:phoenix, github: "phoenixframework/phoenix"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"}]
